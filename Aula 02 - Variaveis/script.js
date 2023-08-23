c6a23ef80a84201cc3a93c315c543ddc3d8aae4e
Gustavo <106829052+GustavoKehr@users.noreply.github.com>
2023-08-23 18:16:20 -0300

    add: new activities

---
+++ Aula 02 - Variaveis/script.js
@@ -0,0 +1,114 @@
+// // tipos primitivos
+
+// //boolean
+// var torf = false;
+// console.log(typeof(torf));
+
+// //number 
+
+// var numberR = 1;
+// console.log(typeof(numberR));
+
+// //string
+
+// var stringR = "suarez";
+// console.log(typeof(stringR));
+
+// //function
+
+// var functionR = function() {}
+// console.log(typeof(functionR));
+
+// // como declarar
+
+// var variavel = "iniesta"; 
+// variavel = "xavi"
+// console.log(variavel)
+
+// let variavel2 = "jordi-alba";
+// variavel2 = "valdez"
+// console.log(variavel2)
+
+// const constante = "Lionel-Messi"
+// console.log(constante)
+
+
+// var escopog = 'global';
+// console.log(escopog);
+
+// function escopoL() {
+//     let escopol_interno = 'local';
+//     console.log(escopol_interno)
+// }
+
+// escopoL();
+
+
+/* // atribuição
+var atribuicao = 'jjj';
+
+// comparação
+var comparacao = "0" == 0;
+console.log(comparacao);
+
+// comparação identica
+var comparacaoIdentica = "0" === 0;
+console.log(comparacaoIdentica); */
+
+
+/*  // operadores aritméticos
+// adição
+var adicao = 1 + 1;
+console.log(adicao);
+
+//subtração
+var subtracao = 2 - 1;
+console.log(subtracao);
+
+//multiplicação
+var multiplicacao = 2 * 3;
+console.log(multiplicacao);
+
+//divisão real
+var divisaoReal = 6 / 2;
+console.log(divisaoReal);
+
+//divisão inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);;
+
+//potenciação
+var potenciacao = 2**10;
+console.log(potenciacao); */
+
+
+/*  // operadores relacionais
+// maior que
+var maiorQue = 5 > 2;
+console.log(maiorQue);
+
+// menor que
+var menorQue = 5 < 2;
+console.log(menorQue);
+
+// maior ou igual a
+var maiorOuIgual = 5 >= 2;
+console.log(maiorOuIgual);
+
+// menor ou igual a
+var menorOuIgual = 5 <= 2;
+console.log(menorOuIgual); */
+
+
+/*  // operadores lógicos
+// && - todos os valores devem ser true
+var e = true && true;
+console.log(e);
+
+// || - somente um valor deve ser true
+var ou = true || false;
+console.log(ou);
+
+// ! - inverte os valores
+var nao = !true;
+console.log(nao); */
---
+++ Aula 06 - Aprofundando em funcoes/calc.js
@@ -0,0 +1,4 @@
+function calculadora() {
+    const operacao = prompt('Escolha uma operacao\n1- Soma\n2- Subtracao\n3- Multiplicacao');
+}
+calculadora();
